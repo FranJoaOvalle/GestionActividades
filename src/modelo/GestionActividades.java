f50d547e274ad923418719a6116c4bf8b7bb3d9c
OscarAlbornoz <oscatalbornoz@gmail.com>
2025-11-27 00:24:08 -0300

    Agregue funciones de actualizar

--- src/modelo/GestionActividades.java
+++ src/modelo/GestionActividades.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -86,6 +88,100 @@ public class GestionActividades {
             }
         }
         return eliminado;
+    }
+
+    public boolean actualizarActividad(Actividad antiguosDatos, Actividad nuevosDatos){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == antiguosDatos.getIdActividad()){
+                a.setNombreActividad(nuevosDatos.getNombreActividad());
+                a.setDescripcion(nuevosDatos.getDescripcion());
+                a.setDia(nuevosDatos.getDia());
+                a.setHoraInicio(nuevosDatos.getHoraInicio());
+                a.setHoraFin(nuevosDatos.getHoraFin());
+                a.setCupos(nuevosDatos.getCupos());
+                a.setNombreProfesor(nuevosDatos.getNombreProfesor());
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarNombreActividad(Actividad actividad, String nombre){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setNombreActividad(nombre);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarDescripcionActividad(Actividad actividad, String descripcion){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setDescripcion(descripcion);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarDiaActividad(Actividad actividad, DayOfWeek dia){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setDia(dia);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarHoraInicioActividad(Actividad actividad, LocalTime horaInicio){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setHoraInicio(horaInicio);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarHoraFinActividad(Actividad actividad, LocalTime horaFin){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setHoraFin(horaFin);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarCupos(Actividad actividad, int cupos){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setCupos(cupos);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarNombreProfesor(Actividad actividad, String nombreProfesor){
+        for(Actividad a: actividades){
+            if(a.getIdActividad() == actividad.getIdActividad()){
+                a.setNombreProfesor(nombreProfesor);
+
+                return true;
+            }
+        }
+        return false;
     }
 
     public ArrayList<Actividad> getActividades() {
